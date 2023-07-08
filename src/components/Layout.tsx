49b60b6eb6704da4f004b95884207d8be3913ee9
권가은 <aileen1007@ewhain.net>
2023-07-09 04:47:12 +0900

    feat: next/font 적용 (ClashDisplayRegular)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,24 @@
 import Header from './Header';
 
+import localFont from 'next/font/local';
+
+// const AppleSDGothicNeo = localFont({
+//   src: '../../public/fonts/AppleSDGothicNeoM.ttf',
+//   display: 'swap',
+//   variable: '--default-font',
+// });
+
+const ClashDisplayRegular = localFont({
+  src: '../../public/fonts/ClashDisplay-Regular.otf',
+  display: 'swap',
+  variable: '--logo-font',
+});
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <div className={`${ClashDisplayRegular.variable}`}>
       <Header />
-      <div>
-        <main>{children}</main>
-      </div>
-    </>
+      <main>{children}</main>
+    </div>
   );
 }
