30714cd0b9d11afdf2d9b5e65cd2028944e45110
권가은 <aileen1007@ewhain.net>
2023-07-11 21:32:20 +0900

    fix: Buttons 컴포넌트들 수정

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,11 +1,15 @@
 interface buttonProps {
-  icon?: React.ReactElement;
+  icon?: React.ReactNode;
   text: string;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
-  disabled: boolean;
+  disabled?: boolean;
 }
 
-export const ButtonLarge = ({ text, onClick, disabled }: buttonProps) => {
+export const ButtonLarge = ({
+  text,
+  onClick,
+  disabled = false,
+}: buttonProps) => {
   return (
     <button
       className={`w-[369px] h-[62px] text-heading3 rounded-[48px] ${
@@ -18,7 +22,11 @@ export const ButtonLarge = ({ text, onClick, disabled }: buttonProps) => {
   );
 };
 
-export const ButtonMedium = ({ text, onClick, disabled }: buttonProps) => {
+export const ButtonMedium = ({
+  text,
+  onClick,
+  disabled = false,
+}: buttonProps) => {
   return (
     <button
       className={`w-[321px] h-[62px] text-heading3 rounded-[48px] ${
@@ -31,12 +39,12 @@ export const ButtonMedium = ({ text, onClick, disabled }: buttonProps) => {
   );
 };
 
-export const ButtonSmall = ({ icon, text, onClick, disabled }: buttonProps) => {
+export const ButtonSmall = ({ icon, text, onClick }: buttonProps) => {
   return (
     <button
-      className={`w-[109px] h-[40px] text-heading3 rounded-[8px] flex justify-center items-center gap-[8px] ${
-        disabled ? 'text-white bg-gray-6' : 'text-black bg-gray-9'
-      }`}
+      className={
+        'w-fit y-fit text-[14px] px-[10px] py-[8px] rounded-[8px] flex justify-center items-center gap-[8px] text-white bg-gray-9'
+      }
       onClick={onClick}
     >
       {icon}
