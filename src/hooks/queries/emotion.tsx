82e8b42bd9f539b74d2035ec29418d57d51f2af7
권가은 <aileen1007@ewhain.net>
2023-08-04 11:58:01 +0900

    wip

--- src/hooks/queries/emotion.tsx
+++ src/hooks/queries/emotion.tsx
@@ -1,8 +1,26 @@
 import HTTP from '@/utils/HTTP';
 import { IEmotion } from '@/interfaces/interfaces';
 
+// 감정 리스트 조회
 export const getEmotionRecordList = async (counseleeId: number | string) => {
   const response = await HTTP.get(`/emotion/list/${counseleeId}/`);
+
+  return response.data;
+};
+
+// 감정 그래프 조회
+export const getEmotionGraph = async (counseleeId: number | string) => {
+  const response = await HTTP.get(`/emotion/graph/${counseleeId}/`);
+
+  return response.data;
+};
+
+// 감정 생성
+export const postEmotionLog = async (
+  counseleeId: number | string,
+  body: any,
+) => {
+  const response = await HTTP.post(`/emotion/${counseleeId}/`, body);
 
   return response.data;
 };
