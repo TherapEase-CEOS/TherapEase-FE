cc1eee097938ee5d98282c1849d2b90bb00022d0
flowersayo <flowersayo0811@ewhain.net>
2025-05-23 20:15:24 +0900

    feat: 새로고침 후 로그인 유지

--- src/store/user.ts
+++ src/store/user.ts
@@ -1,9 +1,15 @@
 import { atom, atomFamily } from 'recoil';
 import { IClient, IUser } from '@/interfaces/interfaces';
+import { recoilPersist } from 'recoil-persist';
 
+const { persistAtom } = recoilPersist({
+  key: 'userPersist', // localStorage 키 이름
+  storage: typeof window !== 'undefined' ? localStorage : undefined,
+});
 export const userState = atom<IUser | null>({
   key: 'userState',
   default: null,
+  effects_UNSTABLE: [persistAtom], // ✅ persist 연결
 });
 
 export const clientsListState = atom<IClient[]>({
